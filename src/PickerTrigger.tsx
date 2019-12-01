1640fffd1492ab808c552efd0e535d2204f02c9b
zombiej <smith3816@gmail.com>
2019-12-01 16:35:18 +0800

    feat: Auto switch popup placement

--- src/PickerTrigger.tsx
+++ src/PickerTrigger.tsx
@@ -7,12 +7,28 @@ const BUILT_IN_PLACEMENTS = {
     points: ['tl', 'bl'],
     offset: [0, 4],
     overflow: {
-      adjustX: 0,
+      adjustX: 1,
+      adjustY: 1,
+    },
+  },
+  bottomRight: {
+    points: ['tr', 'br'],
+    offset: [0, 4],
+    overflow: {
+      adjustX: 1,
       adjustY: 1,
     },
   },
   topLeft: {
     points: ['bl', 'tl'],
+    offset: [0, -4],
+    overflow: {
+      adjustX: 0,
+      adjustY: 1,
+    },
+  },
+  topRight: {
+    points: ['br', 'tr'],
     offset: [0, -4],
     overflow: {
       adjustX: 0,
