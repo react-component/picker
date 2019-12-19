f407a4f4aa5aa543dba447a954d026f8cc4972ce
iamkun <kunhello@outlook.com>
2019-12-19 11:31:45 +0800

    fix: update dayjs generate

--- src/generate/dayjs.ts
+++ src/generate/dayjs.ts
@@ -1,15 +1,27 @@
 import dayjs, { Dayjs } from 'dayjs';
 import { noteOnce } from 'rc-util/lib/warning';
-import weekday from 'dayjs/plugin/weekday'
-import localeData from 'dayjs/plugin/localeData'
-import weekOfYear from 'dayjs/plugin/weekOfYear'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
+import weekday from 'dayjs/plugin/weekday';
+import localeData from 'dayjs/plugin/localeData';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { GenerateConfig } from '.';
 
 dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
 dayjs.extend(weekday);
 dayjs.extend(localeData);
 dayjs.extend(weekOfYear);
+
+dayjs.extend((o, c) => {
+  // todo support Wo (ISO week)
+  const proto = c.prototype;
+  const oldFormat = proto.format;
+  proto.format = function(formatStr: string) {
+    const str = formatStr.replace('Wo', 'wo');
+    return oldFormat.bind(this)(str);
+  };
+});
 
 type IlocaleMapObject = { [key: string]: string };
 const localeMap: IlocaleMapObject = {
@@ -17,12 +29,12 @@ const localeMap: IlocaleMapObject = {
   en_US: 'en',
   zh_CN: 'zh-cn',
   zh_TW: 'zh-tw',
-}
+};
 
 const parseLocale = (locale: string) => {
-  const mapLocale = localeMap[locale]
-  return mapLocale || locale.split('_')[0]
-}
+  const mapLocale = localeMap[locale];
+  return mapLocale || locale.split('_')[0];
+};
 
 const generateConfig: GenerateConfig<Dayjs> = {
   // get
@@ -56,9 +68,18 @@ const generateConfig: GenerateConfig<Dayjs> = {
       return date.localeData().firstDayOfWeek();
     },
     getWeek: (locale, date) => date.locale(parseLocale(locale)).week(),
-    getShortWeekDays: locale => dayjs().locale(parseLocale(locale)).localeData().weekdaysMin(),
-    getShortMonths: locale => dayjs().locale(parseLocale(locale)).localeData().monthsShort(),
-    format: (locale, date, format) => date.locale(parseLocale(locale)).format(format),
+    getShortWeekDays: locale =>
+      dayjs()
+        .locale(parseLocale(locale))
+        .localeData()
+        .weekdaysMin(),
+    getShortMonths: locale =>
+      dayjs()
+        .locale(parseLocale(locale))
+        .localeData()
+        .monthsShort(),
+    format: (locale, date, format) =>
+      date.locale(parseLocale(locale)).format(format),
     parse: (locale, text, formats) => {
       const fallbackFormatList: string[] = [];
 
