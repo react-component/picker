70a326931ce362e6074db6835a1f2d7069a85d29
zombiej <smith3816@gmail.com>
2020-06-03 11:54:53 +0800

    adjust week disabled logic

--- src/hooks/useRangeDisabled.ts
+++ src/hooks/useRangeDisabled.ts
@@ -3,7 +3,6 @@ import { RangeValue, PickerMode, Locale } from '../interface';
 import { getValue } from '../utils/miscUtil';
 import { GenerateConfig } from '../generate';
 import { isSameDate } from '../utils/dateUtil';
-import useWeekDisabled from './useWeekDisabled';
 
 export default function useRangeDisabled<DateType>({
   picker,
@@ -30,10 +29,7 @@ export default function useRangeDisabled<DateType>({
       }
 
       if (disabled[1] && endDate) {
-        return (
-          !isSameDate(generateConfig, date, endDate) &&
-          generateConfig.isAfter(date, endDate)
-        );
+        return !isSameDate(generateConfig, date, endDate) && generateConfig.isAfter(date, endDate);
       }
 
       return false;
@@ -48,12 +44,15 @@ export default function useRangeDisabled<DateType>({
       }
 
       if (startDate) {
-        const compareStartDate =
-          picker === 'week' ? generateConfig.addDate(startDate, -7) : startDate;
+        if (picker === 'week') {
+          const startWeek = generateConfig.locale.getWeek(locale.locale, startDate);
+          const dateWeek = generateConfig.locale.getWeek(locale.locale, date);
+
+          return dateWeek < startWeek;
+        }
 
         return (
-          !isSameDate(generateConfig, date, compareStartDate) &&
-          generateConfig.isAfter(compareStartDate, date)
+          !isSameDate(generateConfig, date, startDate) && generateConfig.isAfter(startDate, date)
         );
       }
 
@@ -61,25 +60,6 @@ export default function useRangeDisabled<DateType>({
     },
     [disabledDate, startDate, picker],
   );
-
-  // Handle week date disabled
-  const sharedWeekDisabledConfig = {
-    generateConfig,
-    locale,
-  };
-
-  const [disabledStartWeekDate] = useWeekDisabled({
-    ...sharedWeekDisabledConfig,
-    disabledDate: disabledStartDate,
-  });
-  const [disabledEndWeekDate] = useWeekDisabled({
-    ...sharedWeekDisabledConfig,
-    disabledDate: disableEndDate,
-  });
-
-  if (picker === 'week') {
-    return [disabledStartWeekDate, disabledEndWeekDate];
-  }
 
   return [disabledStartDate, disableEndDate];
 }
