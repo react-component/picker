c3724af91ad9a977981a0dd31e04daa40eb90b71
二货机器人 <smith3816@gmail.com>
2020-07-07 23:14:08 +0800

    feat: support  panelRender (#100)
    * feat: support panelRender
    
    * update demo
    
    * update test case

---
+++ examples/panelRender.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import moment, { Moment } from 'moment';
+import Picker from '../src/Picker';
+import RangePicker from '../src/RangePicker';
+import momentGenerateConfig from '../src/generate/moment';
+import zhCN from '../src/locale/zh_CN';
+import '../assets/index.less';
+import './common.less';
+
+const defaultStartValue = moment('2019-09-03 05:02:03');
+const defaultEndValue = moment('2019-11-28 01:02:03');
+const defaultValue: [Moment, Moment] = [defaultStartValue, defaultEndValue];
+
+export default () => {
+  const [customizeNode, setCustomizeNode] = React.useState(false);
+
+  return (
+    <>
+      {String(customizeNode)}
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div>
+          <h3>Picker</h3>
+          <Picker<Moment>
+            generateConfig={momentGenerateConfig}
+            locale={zhCN}
+            allowClear
+            defaultValue={defaultStartValue}
+            panelRender={node => (
+              <>
+                <button
+                  type="button"
+                  style={{ display: 'block' }}
+                  onClick={() => {
+                    setCustomizeNode(!customizeNode);
+                  }}
+                >
+                  Change
+                </button>
+
+                {customizeNode ? <span>My Panel</span> : node}
+              </>
+            )}
+          />
+        </div>
+        <div>
+          <h3>RangePicker</h3>
+          <RangePicker<Moment>
+            generateConfig={momentGenerateConfig}
+            locale={zhCN}
+            allowClear
+            defaultValue={defaultValue}
+            panelRender={node => (
+              <>
+                <button
+                  type="button"
+                  style={{ display: 'block' }}
+                  onClick={() => {
+                    setCustomizeNode(!customizeNode);
+                  }}
+                >
+                  Change
+                </button>
+                {customizeNode ? <span>My Panel</span> : node}
+              </>
+            )}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
