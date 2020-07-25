81387b88d22cc3757fa958cc8bb1a3a45ffe64c0
zgo <zguoby@gmail.com>
2020-07-25 17:14:07 +0800

    mockDate

--- tests/panel.spec.tsx
+++ tests/panel.spec.tsx
@@ -198,99 +198,68 @@ describe('Picker.Panel', () => {
   });
 
   // This test is safe to remove
-  describe('showtime', () => {
-    it('object', () => {
-      const onSelect = jest.fn();
-      const wrapper = mount(
-        <MomentPickerPanel
-          showTime={{
-            hideDisabledOptions: true,
-            showSecond: false,
-            defaultValue: getMoment('2001-01-02 01:03:07'),
-            disabledHours: () => [0, 1, 2, 3],
-          }}
-          defaultValue={getMoment('2001-01-02 01:03:07')}
-          value={null}
-          onSelect={onSelect}
-        />,
-      );
+  it('showtime', () => {
+    const onSelect = jest.fn();
+    const wrapper = mount(
+      <MomentPickerPanel
+        showTime={{
+          hideDisabledOptions: true,
+          showSecond: false,
+          defaultValue: getMoment('2001-01-02 01:03:07'),
+          disabledHours: () => [0, 1, 2, 3],
+        }}
+        defaultValue={getMoment('2001-01-02 01:03:07')}
+        value={null}
+        onSelect={onSelect}
+      />,
+    );
 
-      expect(wrapper.find('.rc-picker-time-panel-column')).toHaveLength(2);
-      expect(
-        wrapper
-          .find('.rc-picker-time-panel-column')
-          .first()
-          .find('li')
-          .first()
-          .text(),
-      ).toEqual('04');
-
-      // Click on date
-      wrapper.selectCell(5);
-      expect(isSame(onSelect.mock.calls[0][0], '1990-09-05 01:03:07')).toBeTruthy();
-
-      // Click on time
-      onSelect.mockReset();
+    expect(wrapper.find('.rc-picker-time-panel-column')).toHaveLength(2);
+    expect(
       wrapper
-        .find('ul')
+        .find('.rc-picker-time-panel-column')
         .first()
         .find('li')
-        .at(11)
-        .simulate('click');
-      expect(isSame(onSelect.mock.calls[0][0], '2001-01-02 11:00:00')).toBeTruthy();
-    });
-
-    it('object have no defaultValue', () => {
-      const onSelect = jest.fn();
-      const wrapper = mount(
-        <MomentPickerPanel
-          showTime={{}}
-          defaultValue={getMoment('2001-01-02 01:03:07')}
-          value={null}
-          onSelect={onSelect}
-        />,
-      );
-
-      // Click on date
-      wrapper.selectCell(8);
-      expect(
-        isSame(onSelect.mock.calls[0][0], `1990-09-08 ${moment().format('hh:mm:ss')}`),
-      ).toBeTruthy();
-
-      // Click on time
-      onSelect.mockReset();
-      wrapper
-        .find('ul')
         .first()
-        .find('li')
-        .at(10)
-        .simulate('click');
-      expect(
-        isSame(onSelect.mock.calls[0][0], `2001-01-02 10:${moment().format('mm:ss')}`),
-      ).toBeTruthy();
-    });
+        .text(),
+    ).toEqual('04');
 
-    it('true', () => {
-      const onSelect = jest.fn();
-      const wrapper = mount(<MomentPickerPanel showTime onSelect={onSelect} />);
+    // Click on date
+    wrapper.selectCell(5);
+    expect(isSame(onSelect.mock.calls[0][0], '1990-09-05 01:03:07')).toBeTruthy();
 
-      // Click on date
+    // Click on time
+    onSelect.mockReset();
+    wrapper
+      .find('ul')
+      .first()
+      .find('li')
+      .at(11)
+      .simulate('click');
+    expect(isSame(onSelect.mock.calls[0][0], '2001-01-02 11:00:00')).toBeTruthy();
+  });
+
+  // This test is safe to remove
+  it('showtime is true', () => {
+    const onSelect = jest.fn();
+    const wrapper = mount(<MomentPickerPanel showTime value={null} onSelect={onSelect} />);
+
+    const timerGame = new Promise(resolve =>
+      setTimeout(() => {
+        MockDate.set(getMoment('1990-09-03 01:10:10').toDate());
+        resolve();
+      }, 1000),
+    );
+
+    // Click on date
+    wrapper.selectCell(5);
+    expect(isSame(onSelect.mock.calls[0][0], `1990-09-05 00:00:00`, 'seconds')).toBeTruthy();
+
+    // Click on date
+    timerGame.then(() => {
       wrapper.selectCell(10);
       expect(
-        isSame(onSelect.mock.calls[0][0], `1990-09-10 ${moment().format('hh:mm:ss')}`),
-      ).toBeTruthy();
-
-      // Click on time
-      onSelect.mockReset();
-      wrapper.selectCell(8);
-      wrapper
-        .find('ul')
-        .first()
-        .find('li')
-        .at(10)
-        .simulate('click');
-      expect(
-        isSame(onSelect.mock.calls[0][0], `1990-09-08 10:${moment().format('mm:ss')}`),
+        isSame(onSelect.mock.calls[0][0], `1990-09-10 ${moment().format('hh:mm:ss')}`, 'seconds'),
       ).toBeTruthy();
     });
   });
