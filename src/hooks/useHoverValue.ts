c0afab51b48b61fd2d0d3937f367bdd37809d822
Kermit <kermitlx@outlook.com>
2020-09-07 21:10:54 +0800

    chore(useHoverValue): add raf to optimize performance

--- src/hooks/useHoverValue.ts
+++ src/hooks/useHoverValue.ts
@@ -1,11 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import useValueTexts, { ValueTextConfig } from './useValueTexts';
 
 export default function useHoverValue<DateType>(
   valueText: string,
   { formatList, generateConfig, locale }: ValueTextConfig<DateType>,
 ): [string, (date: DateType) => void, (date: DateType) => void] {
-  const [value, setValue] = useState(null);
+  const [value, internalSetValue] = useState<DateType>(null);
+  const raf = useRef(null);
+
+  function setValue(val: DateType, immediately: boolean = false) {
+    cancelAnimationFrame(raf.current);
+    if (immediately) {
+      internalSetValue(val);
+      return;
+    }
+    raf.current = requestAnimationFrame(() => {
+      internalSetValue(val);
+    });
+  }
 
   const [, firstText] = useValueTexts(value, {
     formatList,
@@ -24,6 +36,8 @@ export default function useHoverValue<DateType>(
   useEffect(() => {
     onLeave();
   }, [valueText]);
+
+  useEffect(() => () => cancelAnimationFrame(raf.current), []);
 
   return [firstText, onEnter, onLeave];
 }
