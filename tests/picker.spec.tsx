dbe843b8c47a9b66d6db84aaec2db1a1fefc06ef
Kermit <kermitlx@outlook.com>
2020-12-28 22:22:25 +0800

    chore: add test case

--- tests/picker.spec.tsx
+++ tests/picker.spec.tsx
@@ -893,4 +893,67 @@ describe('Picker.Basic', () => {
       expect(wrapper.isOpen()).toBeFalsy();
     });
   });
+
+  describe('disabledDate', () => {
+    function disabledDate(current: Moment) {
+      return current <= getMoment('2020-12-28 00:00:00').endOf('day');
+    }
+    const wrapper = mount(<MomentPicker open defaultValue={getMoment('2020-12-29 12:00:00')} disabledDate={disabledDate} />);
+    // Date Panel
+    Array.from({
+      length: 31
+    }).forEach((v, i) => {
+      const cell = wrapper.findCell(`${i + 1}`);
+      // >= 29
+      if (i >= 28) {
+        expect(cell.hasClass('rc-picker-cell-disabled')).toBeFalsy();
+      } else {
+        expect(cell.hasClass('rc-picker-cell-disabled')).toBeTruthy();
+      }
+    });
+    wrapper.find('.rc-picker-month-btn').simulate('click');
+    // Month Panel
+    Array.from({
+      length: 12
+    }).forEach((v, i) => {
+      const cell = wrapper.find('.rc-picker-cell-in-view').at(i);
+      // >= 12
+      if (i >= 11) {
+        expect(cell.hasClass('rc-picker-cell-disabled')).toBeFalsy();
+      } else {
+        expect(cell.hasClass('rc-picker-cell-disabled')).toBeTruthy();
+      }
+    });
+    wrapper.find('.rc-picker-year-btn').simulate('click');
+    // Year Panel
+    Array.from({
+      length: 10
+    }).forEach((v, i) => {
+      const cell = wrapper.find('.rc-picker-cell-in-view').at(i);
+      // >= 2020
+      expect(cell.hasClass('rc-picker-cell-disabled')).toBeFalsy();
+    });
+    // Decade Panel
+    Array.from({
+      length: 8
+    }).forEach((v, i) => {
+      const cell = wrapper.find('.rc-picker-cell-in-view').at(i);
+      // >= 2020
+      expect(cell.hasClass('rc-picker-cell-disabled')).toBeFalsy();
+    });
+
+    const quarterWrapper = mount(<MomentPicker picker="quarter" open defaultValue={getMoment('2020-12-29 12:00:00')} disabledDate={disabledDate} />);
+    // quarter Panel
+    Array.from({
+      length: 4
+    }).forEach((v, i) => {
+      const cell = quarterWrapper.find('.rc-picker-cell-in-view').at(i);
+      // >= 4
+      if (i >= 3) {
+        expect(cell.hasClass('rc-picker-cell-disabled')).toBeFalsy();
+      } else {
+        expect(cell.hasClass('rc-picker-cell-disabled')).toBeTruthy();
+      }
+    });
+  });
 });
