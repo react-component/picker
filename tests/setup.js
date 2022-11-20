ed917e98591c79de648fc63977adc3a4edf6559c
二货机器人 <smith3816@gmail.com>
2022-11-20 12:54:25 +0800

    chore: adjust adaptaer

--- tests/setup.js
+++ tests/setup.js
@@ -7,7 +7,7 @@ global.cancelAnimationFrame = id => {
 };
 
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
 Enzyme.configure({ adapter: new Adapter() });
 
