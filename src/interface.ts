829ac93ac12f0846e7dedb59ff8abf41fe7a1616
wuxh <wxh1220@gmail.com>
2023-02-27 17:28:26 +0800

    feat(type): update TS type

--- src/interface.ts
+++ src/interface.ts
@@ -112,3 +112,10 @@ export interface PresetDate<T> {
   label: React.ReactNode;
   value: T;
 }
+
+// https://stackoverflow.com/a/39495173; need TypeScript >= 4.5
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>
+
+export type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
