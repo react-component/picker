5afd1074cd19cd67e800fb187189d7fdd0aaac22
wuxh <wxh1220@gmail.com>
2023-10-20 23:55:32 +0800

    chore: add test

--- tests/picker.spec.tsx
+++ tests/picker.spec.tsx
@@ -1145,4 +1145,62 @@ describe('Picker.Basic', () => {
 
     expect(container.querySelector('input')).toHaveValue('2023-09-04 21:05:10');
   });
+
+
+
+
+
+  describe.only('Picker format by locale', () => {
+    const myLocale = {
+      ...zhCN,
+      dateFormat: 'YYYY 年 M 月 D 日',
+      dateTimeFormat: 'YYYY 年 M 月 D 日 H 时 m 分 s 秒',
+      weekFormat: 'YYYY 年 W 周',
+      monthFormat: 'YYYY 年 M 月',
+    };
+
+    const date = moment('2000-01-01', 'YYYY-MM-DD');
+    function matchPicker(name: string, props?: any) {
+      it(name, () => {
+        const { container } = render(
+          <MomentPicker value={date} {...props} locale={myLocale} />
+        );
+        expect(container.firstChild).toMatchSnapshot();
+      });
+    }
+
+    matchPicker('date');
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
