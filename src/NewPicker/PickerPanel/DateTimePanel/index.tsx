3c96f62d1106c96ddb3e033b076b12ae49838eec
二货机器人 <smith3816@gmail.com>
2023-10-25 17:04:52 +0800

    chore: all panels

--- src/NewPicker/PickerPanel/DateTimePanel/index.tsx
+++ src/NewPicker/PickerPanel/DateTimePanel/index.tsx
@@ -4,17 +4,15 @@ import DatePanel from '../DatePanel';
 import TimePanel from '../TimePanel';
 
 export default function DateTimePanel<DateType = any>(props: SharedPanelProps<DateType>) {
-  const { prefixCls } = props;
+  const { prefixCls, value, pickerValue } = props;
 
   const panelPrefixCls = `${prefixCls}-datetime-panel`;
-
-  console.log('??>', props);
 
   // ============================== Render ==============================
   return (
     <div className={panelPrefixCls}>
       <DatePanel {...props} />
-      <TimePanel {...props} />
+      <TimePanel {...props} value={value || pickerValue} />
     </div>
   );
 }
