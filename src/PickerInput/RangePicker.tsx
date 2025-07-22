cc1075ab1ddd6fbe571081b1c057bc870c6df5cb
zhengjihao <2714499297@qq.com>
2025-07-22 18:21:32 +0800

    fix: add 'preset-click' operation type to RangePicker

--- src/PickerInput/RangePicker.tsx
+++ src/PickerInput/RangePicker.tsx
@@ -495,6 +495,7 @@ function RangePicker<DateType extends object = any>(
     const passed = triggerSubmitChange(nextValues);
 
     if (passed) {
+      lastOperation('preset-click');
       triggerOpen(false, { force: true });
     }
   };
